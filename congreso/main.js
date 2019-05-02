37c1d791f8f8ff81817e739632648410018cb6dd
osvaldo345 <42743246+osvaldo345@users.noreply.github.com>
2019-05-01 19:00:01 -0500

    avance de javascript

---
+++ congreso/main.js
@@ -0,0 +1,53 @@
+import Talleres from "/talleres.js";
+import Participantes from "/Participantes.js";
+
+class Main {
+  constructor() {
+    let agenda = new Talleres(
+      document.querySelector("#agenda"),
+      document.querySelector("#info")
+    );
+
+    document.querySelector("#btnAdd").addEventListener("click", () => {
+      let form = document.querySelector("#form");
+      form.classList.add("was-validated");
+
+      if (form.checkValidity() === true) {
+        let id = document.querySelector("#id").value;
+        let sBirthday = document.querySelector("#birthday").value;
+        sBirthday = sBirthday.split("-");
+
+        let birthday = new Date(sBirthday[0], sBirthday[1] - 1, sBirthday[2]);
+
+        let sBirthday2 = document.querySelector("#birthday2").value;
+        sBirthday2 = sBirthday2.split("-");
+
+        let birthday2 = new Date(sBirthday2[0], sBirthday2[1] - 1, sBirthday2[2]);
+
+
+        let lugares = document.querySelector("#lugares").value;
+        let duracion = document.querySelector("#duracion").value;
+
+        let objParticipante = {
+          id: id,
+          birthday: birthday,
+          birthday2: birthday2,
+          lugares: lugares,
+          duracion: duracion
+        };
+
+        let Participante = new Participantes(objParticipante);
+
+        talleres.addEmployee(participante);
+
+      }
+
+      console.log(form.checkValidity());
+
+
+
+    });
+  }
+}
+
+let m = new Main();
