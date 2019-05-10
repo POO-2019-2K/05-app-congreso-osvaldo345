34c830ba6e6c73e4038cff54b6a06a2080f28d58
osvaldo345 <42743246+osvaldo345@users.noreply.github.com>
2019-05-09 19:39:30 -0500

    mejora del localstorage

--- congreso/talleres.js
+++ congreso/talleres.js
@@ -20,6 +20,7 @@ export default class Talleres {
     taller.forEach( (Participante, index) => {
       console.log(Participante);
       Participante.birthday = new Date(Participante.birthday);
+      Participante.birthday2 = new Date(Participante.birthday2);
       this._showInTable(new Participantes(Participante));
     });
   }
@@ -29,7 +30,7 @@ export default class Talleres {
     let cellBirthday = row.insertCell(1);
     let cellBirthday2 = row.insertCell(2);
     let cellLugares = row.insertCell(3);
-    let cellDuracion = row.insertCell(4);
+    let cellDuracion = row.insertCell(4); 
 
     cellId.innerHTML = participante.id;
     cellBirthday.innerHTML = participante.getBirthdayAsString();
@@ -45,13 +46,35 @@ export default class Talleres {
       duracion: participante.duracion
     }
     this._taller.push(objParticipante);
-
-
   }
+    
+    
+    _findId(id){
+      let found = -1
+  
+      this._taller.forEach((participante, index)=>{
+          if(participante.id === id){
+              found = index;
+              return;
+          }
+      });
+      return found;
+  }
+  
+  
+  
   addEmployee(participante) {
-    this._showInTable(participante);
-    localStorage.setItem("taller", JSON.stringify(this._taller));
-    console.log(localStorage.getItem("taller"));
+      let found = this._findId(participante.id);
+      if (found >= 0){
+          swal.fire({
+              type: "error",
+              tittle: "error",
+              text: "este taller ya esta registrado"
+          });
+          return;
+      }
+      this._showInTable(participante);
+      localStorage.setItem("taller", JSON.stringify(this._taller));
+      console.log(localStorage.getItem("taller"));
   }
-}
-
+  }
